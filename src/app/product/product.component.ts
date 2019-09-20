434eb522392ee20ee3e843f7c9646c7930abfe3a
Carlos Arbildo <cosner1@gmail.com>
2019-09-19 20:14:20 -0500

    se agregan peticiones con axios

--- src/app/product/product.component.ts
+++ src/app/product/product.component.ts
@@ -1,16 +1,18 @@
 import { Component, OnInit } from '@angular/core';
 import { ProductService } from './product.service';
 
+
+let params = [];
+
 @Component({
   selector: 'app-product',
   templateUrl: './product.component.html',
   styleUrls: ['./product.component.css']
 })
-
-
 export class ProductComponent implements OnInit {
-  constructor(private productService: ProductService) { }
-  products = this.productService.getProductList();
+  constructor(private productService: ProductService) {
+  }
+  products = this.productService.getProductList(params);
 
   ngOnInit() {
   }
--- src/app/product/product.service.ts
+++ src/app/product/product.service.ts
@@ -1,21 +1,31 @@
 import {Injectable} from '@angular/core';
-import {HttpClient, HttpHeaders} from '@angular/common/http';
 import {Product} from './product';
+import {RestService} from "../rest.service";
 
 @Injectable({
   providedIn: 'root'
 })
+
 export class ProductService {
   baseUrl = 'http://127.0.0.1:8000/tbl/producto/';
-
-  constructor(private  httpClient: HttpClient) {
+  result = [];
+  constructor(private restService: RestService) {
+    this.restService = restService;
   }
 
-  private products: Product[] = [];
-
-  public getProductList() {
-      return this.httpClient.get(this.baseUrl).subscribe((result: Product[]) => result.forEach((x) => {
-      this.products.push(x);
-    }));
+    public getProductList(params) {
+    console.log("si llego")
+     this.restService.get<Product[]>({
+      url: this.baseUrl,
+    }).then(function (response) {
+       console.log(response)
+      return response;
+     }).catch(function (error) {
+      console.log(error);
+    }).finally(function () {
+      console.log("finalizo")
+    });
   }
+
+
 }
